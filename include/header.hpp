d3f26590aeae7a6378ad6526cba499d3a9ccadf8
Darioshka <56868583+Darioshka@users.noreply.github.com>
2019-12-11 15:50:19 +0300

    1.2

--- include/header.hpp
+++ include/header.hpp
@@ -319,4 +319,32 @@ public:
 
 };
 
+
+int main() {
+    Json test("{\n"
+               "    \"lastname\" : \"Ivanov\",\n"
+               "    \"firstname\" : \"Ivan\",\n"
+               "    \"age\" : 25,\n"
+               "    \"islegal\" : false,\n"
+               "    \"marks\" : [\n"
+               "    \t4,5,5,5,2,3\n"
+               "    ],\n"
+               "    \"address\" : {\n"
+               "    \t\"city\" : \"Moscow\",\n"
+               "        \"street\" : \"Vozdvijenka\"\n"
+               "    }\n"
+               "}");
+
+    test.parse();
+    auto hi = any_cast<Json>(test["marks"]);
+    if (!any_cast<bool>(test. mean_code[3]))
+    {
+        cout << any_cast<int>(hi[4]) << std::endl;
+        cout << any_cast<int>(test["age"]) << std::endl;
+    }
+
+    return 0;
+}
+
+
 #endif // INCLUDE_HEADER_HPP_
