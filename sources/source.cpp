8960d3079f3ecfc7a63fe7e41fd37e40ef89f0f0
Darioshka <56868583+Darioshka@users.noreply.github.com>
2019-12-12 21:02:40 +0300

    1.4

--- sources/source.cpp
+++ sources/source.cpp
@@ -1,3 +1,30 @@
-// Copyright 2018 Your Name <your_email>
+#include <iostream>
+#include "json.hpp"
 
-#include <header.hpp>
+
+int main() {
+
+    Json data("{\n"
+               "    \"lastname\" : \"Ivanov\",\n"
+               "    \"firstname\" : \"Ivan\",\n"
+               "    \"age\" : 25,\n"
+               "    \"islegal\" : false,\n"
+               "    \"marks\" : [\n"
+               "    \t4,5,5,5,2,3\n"
+               "    ],\n"
+               "    \"address\" : {\n"
+               "    \t\"city\" : \"Moscow\",\n"
+               "        \"street\" : \"Vozdvijenka\"\n"
+               "    }\n"
+               "}");
+
+    data.parse();
+    auto test = any_cast<Json>(data["marks"]);
+    if (!any_cast<bool>(data.mean_code[3]))
+    {
+        cout << any_cast<int>(test[4]) << std::endl;
+        cout << any_cast<int>(data["age"]) << std::endl;
+    }
+
+    return 0;
+}
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,30 +1,51 @@
+
+
 #include <gtest/gtest.h>
+#include "json.hpp"
+#include <iostream>
+#include <string>
+#include <any>
+#include <map>
+#include <vector>
+#include <sstream>
+#include <fstream>
 
-TEST(Example, EmptyTest) {
-    std::string json = "{
-    \"lastname\" : \"Ivanov\",
-    \"firstname\" : \"Ivan\",
-    \"age\" : 25,
-    \"islegal\" : false,
-    \"marks\" : [
-    	4,5,5,5,2,3
-    ],
-    \"address\" : {
-    	\"city\" : \"Moscow\",
-        \"street\" : \"Vozdvijenka\"
-    }
-}";
-
+TEST(Parse, Text){
+std::string json = "{\n"
+                   "    \"lastname\" : \"Ivanov\",\n"
+                   "    \"firstname\" : \"Ivan\",\n"
+                   "    \"age\" : 25,\n"
+                   "    \"islegal\" : false,\n"
+                   "    \"marks\" : [\n"
+                   "    \t4,5,5,5,2,3\n"
+                   "    ],\n"
+                   "    \"address\" : {\n"
+                   "    \t\"city\" : \"Moscow\",\n"
+                   "        \"street\" : \"Vozdvijenka\"\n"
+                   "    }\n"
+                   "}";
 Json object = Json::parse(json);
-EXPECT_EQ(std::any_cast<std::string>(object["lastname"]), "Ivanov");
-EXPECT_EQ(std::any_cast<bool>(object["islegal"]), false);
-EXPECT_EQ(std::any_cast<int>(object["age"]), 25);
+EXPECT_EQ(std::any_cast<std::string>(object["lastname"]),
+"Ivanov");
+EXPECT_EQ(std::any_cast<bool>(object["islegal"]),
+false);
+EXPECT_EQ(std::any_cast<int>(object["age"]),
+25);
 
 auto marks = std::any_cast<Json>(object["marks"]);
-EXPECT_EQ(std::any_cast<int>(marks[0]), 4);
-EXPECT_EQ(std::any_cast<int>(marks[1]), 5);
+EXPECT_EQ(std::any_cast<int>(marks[0]),
+4);
+EXPECT_EQ(std::any_cast<int>(marks[1]),
+5);
 
 auto address = std::any_cast<Json>(object["address"]);
-EXPECT_EQ(std::any_cast<std::string>(address["city"]), "Moscow");
-EXPECT_EQ(std::any_cast<std::string>(address["street"]), "Vozdvijenka");
+EXPECT_EQ(std::any_cast<std::string>(address["city"]),
+"Moscow");
+EXPECT_EQ(std::any_cast<std::string>(address["street"]),
+"Vozdvijenka");
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
